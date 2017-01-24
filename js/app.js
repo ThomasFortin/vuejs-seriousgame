aeb7ae9db59ca0b49d1592b9021bc7a864200201
alimux <alexducreux14@gmail.com>
2017-01-24 16:24:49 +0100

    adding operation validation

--- js/app.js
+++ js/app.js
@@ -1,42 +1,63 @@
 /*___________________________________________________________
  |															 | 
  |    Serious Game Multiplication Tables for primary v0.1    |
-  ___________________________________________________________*/
+ ___________________________________________________________*/
 
 //Apprentice part
-//tmp table
-var k =1;
+
 var apprenticePart = {
 
-	 template : `#apprenticePart`,
-	 			data : function(){
-	 				return { 
-	 					store : store,
-	 					questionNumber : 0,
-	 					operands : [0,1,2,3,4,5,6,7,8,9,10],
-	 				};
-	 			},
-	 computed:{
-	 	currentOperand: function(){
-	 		return this.operands[this.questionNumber]
-	 	},
-	 	possibleResults: function(){
+	template : `#apprenticePart`,
+	data : function(){
+		return { 
+			store : store,
+			
+			
+		};
+	},
 
-	 		var resultTable = []
-	 		for(var operand in this.operands){
-	 		resultTable.push(operand*store.currentTable)
-	 		}
-	 		console.log(resultTable)
-	 		return resultTable
-	 	}
+	methods : {
+		verifyingResultTemplate : function(arguments){
+			var succeed = false;
+			var currentResult = store.operands[store.questionNumber] * store.currentTable
+			console.log("opération courante : "+ store.operands[store.questionNumber]+ " * "+ store.currentTable  +"\ncurrentResult = "+currentResult+" argument courant : "+ arguments[0])
+			if(currentResult==arguments[0]){
+				succeed = true;
+				console.log("Résultat de l'opération : "+succeed)
+			}
+			else{
+				succeed = false;
+				console.log("Résultat de l'opération : "+succeed)
+			}
+			
+		}
+	},
 
-	 }
+	computed:{
+
+		currentOperand: function(){
+			return store.operands[store.questionNumber]
+		},
+
+		possibleResults: function(){
+
+			var resultTable = []
+			for(var operand in store.operands){
+				resultTable.push(operand*store.currentTable)
+			}
+			console.log(resultTable)
+			return resultTable
+		}
+
+	}
 
 };
 
 var store = {
 	table : [1,2,3,4,5,6,7,8,9,10],
-	currentTable : 5
+	currentTable : 5,
+	questionNumber : 0,
+	operands : [0,1,2,3,4,5,6,7,8,9,10]
 };
 
 Vue.component(`apprentice-part`, apprenticePart);
@@ -52,10 +73,15 @@ new Vue ({
 		// Random method to select 10 operations for one table
 		oneTableCalculate : function(k){
 
-			for(var i=0; i>11;i++){
+			//for(var i=0; i>11;i++){
 
-			}
+			//}
 
+		},
+		verifyingResult : function(){
+			
+			apprenticePart.methods.verifyingResultTemplate(arguments)
+			console.log(arguments[0])
 		}
 
 	}
