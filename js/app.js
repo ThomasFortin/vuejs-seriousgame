25afd1b97ee2930f086d3e5f8f08c7a2b939814b
alimux <alexducreux14@gmail.com>
2017-01-31 08:02:16 +0100

    terminating apprentice module

--- js/app.js
+++ js/app.js
@@ -64,6 +64,16 @@ var apprenticePart = {
 
 };
 
+var totalGame = {
+	template : `#totalGame`,
+	data : function(){
+		return { 
+			store : store						
+		};
+	},
+
+};
+
 //datastore
 var store = {
 	table : [1,2,3,4,5,6,7,8,9,10],
@@ -73,8 +83,7 @@ var store = {
 	operands : [0,1,2,3,4,5,6,7,8,9,10],
 	operandsArray : [],
 	succeed : "",
-	min : Math.ceil(1),
-	max : Math.floor(10),
+	trueAnswer : 0,
 	display : false,
 	win : false,
 
@@ -82,6 +91,7 @@ var store = {
 
 //components
 Vue.component(`apprentice-part`, apprenticePart);
+Vue.component(`total-game`, totalGame);
 
 
 //vue instance
@@ -92,6 +102,16 @@ var test = new Vue ({
 		   
 	methods: {
 
+		//initialize game
+		initGame : function(){
+			store.questionNumber = 1
+			store.succeed = ""
+			store.win = false
+			store.trueAnswer = 0
+			this.randomTable()
+			this.oneTableCalculate()
+		},
+
 		// Random method to select 10 operations for one table
 		oneTableCalculate : function(){
 			var doneOperandsArray = [0]
@@ -99,47 +119,20 @@ var test = new Vue ({
 			var isInArray = true
 			console.log("Table choisie : "+store.currentTable)
 
-			if(store.questionNumber>10){
+			if(store.trueAnswer===11){
 					store.win=true
 					store.succeed=""
 					apprenticePart.methods.setVisible(false)
 			}
 
-			if(store.win==!true && store.questionNumber<10){
+			if(store.win==!true && store.trueAnswer<11){
 
 				//display template
 				apprenticePart.methods.setVisible(true)
 		
-				console.log("Question numéro : "+store.questionNumber)
-				var randomOperand = Math.floor(Math.random() * (store.max - store.min + 1) + store.min)
-				console.log("opérande random = "+randomOperand)
-
-
-				for(i=0;i<doneOperandsArray.length;i++){
-						console.log("Question numéro : "+store.questionNumber)
-						if(doneOperandsArray[i] == randomOperand){
-							console.log(doneOperandsArray[i]+" -> Valeur déjà dans le tableau")
-							isInArray = true
-							break;
-							//return isInArray
-						}
-						else{
-							isInArray = false
-							//return isInArray
-						}
-					}
-					console.log(isInArray)
-					if(isInArray==false){
-						doneOperandsArray.push(randomOperand)
-						store.currentOperand = randomOperand
-						console.log("Opérande courante mis dans le tableau : "+store.currentOperand)
-					}	
-					
+				//random question
+				store.currentOperand = store.operandsArray[store.questionNumber-1]	
 				}
-	
-				console.log("Affichage du tableau : "+doneOperandsArray.length)
-			
-			
 		},
 
 
@@ -148,6 +141,7 @@ var test = new Vue ({
 			if(apprenticePart.methods.goodOrBad(arguments)){
 				store.succeed = true
 				store.questionNumber++
+				store.trueAnswer++
 				this.oneTableCalculate()
 			}
 			else{
@@ -158,9 +152,24 @@ var test = new Vue ({
 		},
 
 		randomTable : function(){
-			var test = Math.random()
-			store.operandsArray = store.operands.sort(test - Math.random())
-			console.log("tableau d'opérande = "+store.operandsArray)
+			//randomize question
+			store.operandsArray = store.operands
+
+			var currentIndex = store.operandsArray.length, temporaryValue, randomIndex;
+
+			  // While there remain elements to shuffle...
+			  while (0 !== currentIndex) {
+
+			    // Pick a remaining element...
+			    randomIndex = Math.floor(Math.random() * currentIndex);
+			    currentIndex -= 1;
+
+			    // And swap it with the current element.
+			    temporaryValue = store.operandsArray[currentIndex];
+			    store.operandsArray[currentIndex] = store.operandsArray[randomIndex];
+			    store.operandsArray[randomIndex] = temporaryValue;
+			  }
+
 		}
 	}
 
